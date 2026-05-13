2cb4d9f8350d5f376540604cd7cdd40d0da639c2
Styrene <imstyrene@tutamail.com>
2026-05-12 22:45:17 -0400

    Added Nixvim

--- flake.nix
+++ flake.nix
@@ -8,16 +8,20 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "pkgs";
     };
+    nixvim.url = "github:nix-community/nixvim";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, nixvim, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations."Styrene" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [ ./home.nix ];
+        modules = [
+          ./home.nix
+          nixvim.homeModules.nixvim
+        ];
       };
     };
 }
