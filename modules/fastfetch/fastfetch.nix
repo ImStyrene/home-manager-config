79a3f50a2325e537840e30788d651a17985977c6
Styrene <imstyrene@tutamail.com>
2026-07-14 19:53:59 -0400

    fastfetch.nix: Uses the new Styrene.txt

--- modules/fastfetch/fastfetch.nix
+++ modules/fastfetch/fastfetch.nix
@@ -8,7 +8,7 @@
 	"$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
 
 	logo = {
-	  source = "~/.config/home-manager/modules/fastfetch/anime-girl-braille.txt";
+	  source = "~/.config/home-manager/modules/fastfetch/styrene.txt.txt";
 	  height = 20;
 	  padding = {
 	    top = 1;
