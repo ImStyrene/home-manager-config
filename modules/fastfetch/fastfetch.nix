ca3007c96eb11570239b332cd4007145acfe6e7d
Styrene <imstyrene@tutamail.com>
2026-08-15 16:07:21 -0400

    fastfetch.nix: Using toki-pona.txt ASCII art

--- modules/fastfetch/fastfetch.nix
+++ modules/fastfetch/fastfetch.nix
@@ -8,7 +8,7 @@
 	"$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
 
 	logo = {
-	  source = "~/.config/home-manager/modules/fastfetch/black-hole-braille.txt";
+	  source = "~/.config/home-manager/modules/fastfetch/toki-pona.txt";
 	  height = 20;
 	  padding = {
 	    top = 1;
